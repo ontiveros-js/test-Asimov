d86a2e12c27a4b5e41b2e9ee5fe5e0e129cfba2d
ontiveros-js <douglasontiveros1408@gmail.com>
2022-06-16 17:08:39 -0400

    Updates

---
+++ precache-manifest.7f02d17e2a74c9dd17d108f6255a72eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4b375d50637a2f86ba2",
+    "url": "/test-Asimov/static/css/main.c15cf8c4.chunk.css"
+  },
+  {
+    "revision": "e4b375d50637a2f86ba2",
+    "url": "/test-Asimov/static/js/main.e4b375d5.chunk.js"
+  },
+  {
+    "revision": "06ed7020fbf189b562b7",
+    "url": "/test-Asimov/static/js/1.06ed7020.chunk.js"
+  },
+  {
+    "revision": "cc66ca9cedefbc412c1b",
+    "url": "/test-Asimov/static/css/2.ae062910.chunk.css"
+  },
+  {
+    "revision": "cc66ca9cedefbc412c1b",
+    "url": "/test-Asimov/static/js/2.cc66ca9c.chunk.js"
+  },
+  {
+    "revision": "e1530fccdd97096008b1",
+    "url": "/test-Asimov/static/js/runtime~main.e1530fcc.js"
+  },
+  {
+    "revision": "b079f887fecdba068701c55169c7e790",
+    "url": "/test-Asimov/index.html"
+  }
+];
